557b37e36158ec73dcc9b7c9c168f9d9ff153221
ccbkv <cs200801005@gmail.com>
2025-08-16 01:47:29 +0800

    Update manhuagui.js

--- manhuagui.js
+++ manhuagui.js
@@ -359,14 +359,9 @@ class ManHuaGui extends ComicSource {
       let params_part = str.split("}(")[1].split("))")[0];
       let params = splitParams(params_part);
       params[5] = {};
-      try {
-        params[3] = LZString.decompressFromBase64(params[3].split("'")[1]).split(
-          "|"
-        );
-      } catch (e) {
-        console.error("解压params[3]失败:", e);
-        params[3] = [];
-      }
+      params[3] = LZString.decompressFromBase64(params[3].split("'")[1]).split(
+        "|"
+      );
       return params;
     }
 
