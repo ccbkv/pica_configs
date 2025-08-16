d7926f7daf67bc3490c68c662d57b82086812123
ccbkv <cs200801005@gmail.com>
2025-08-17 03:29:06 +0800

    Update baihehui.js

--- baihehui.js
+++ baihehui.js
@@ -12,7 +12,7 @@ class Baihehui extends ComicSource {
     minAppVersion = "1.4.0"
 
     // update url
-    url = "https://git.nyne.dev/nyne/venera-configs/raw/branch/main/baihehui.js"
+    url = "https://raw.githubusercontent.com/ccbkv/pica_configs/refs/heads/master/baihehui.js"
 
     settings = {
         domains: {
